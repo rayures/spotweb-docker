86032b0a3743e2627dee6f79a2af24a8d57e977a
rayures <rayures@users.noreply.github.com>
2020-06-10 20:22:26 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -95,34 +95,34 @@ case ${DB_TYPE} in
   ;;
 esac
 
-mkdir /config/
-touch /config/ownsettings.php #&& chown www-data:www-data /config/ownsettings.php
-rm -f /var/www/spotweb/ownsettings.php
-ln -s /config/ownsettings.php /var/www/spotweb/ownsettings.php
+#mkdir /config/
+#touch /config/ownsettings.php #&& chown www-data:www-data /config/ownsettings.php
+#rm -f /var/www/spotweb/ownsettings.php
+#ln -s /config/ownsettings.php /var/www/spotweb/ownsettings.php
 
-if [[ -n "$DB_TYPE" && -n "$DB_HOST" && -n "$DB_NAME" && -n "$DB_USER" && -n "$DB_PASS" ]]; then
-    echo "Creating database configuration"
-    touch /config/dbsettings.inc.php # && chown www-data:www-data ${WebDir}/dbsettings.inc.php
-    echo "<?php" > /config/dbsettings.inc.php
-    echo "\$dbsettings['engine'] = '$DB_TYPE';" >> /config/dbsettings.inc.php
-    echo "\$dbsettings['host'] = '$DB_HOST';" >> /config/dbsettings.inc.php
-    echo "\$dbsettings['dbname'] = '$DB_NAME';"  >> /config/dbsettings.inc.php
-    echo "\$dbsettings['user'] = '$DB_USER';" >> /config/dbsettings.inc.php
-    echo "\$dbsettings['pass'] = '$DB_PASS';"  >> /config/dbsettings.inc.php
-fi
+#if [[ -n "$DB_TYPE" && -n "$DB_HOST" && -n "$DB_NAME" && -n "$DB_USER" && -n "$DB_PASS" ]]; then
+#    echo "Creating database configuration"
+#    touch /config/dbsettings.inc.php # && chown www-data:www-data ${WebDir}/dbsettings.inc.php
+#    echo "<?php" > /config/dbsettings.inc.php
+#    echo "\$dbsettings['engine'] = '$DB_TYPE';" >> /config/dbsettings.inc.php
+#    echo "\$dbsettings['host'] = '$DB_HOST';" >> /config/dbsettings.inc.php
+#    echo "\$dbsettings['dbname'] = '$DB_NAME';"  >> /config/dbsettings.inc.php
+#    echo "\$dbsettings['user'] = '$DB_USER';" >> /config/dbsettings.inc.php
+#    echo "\$dbsettings['pass'] = '$DB_PASS';"  >> /config/dbsettings.inc.php
+#fi
 
-if [[ -n "$DB_PORT" ]]; then
-    echo "adding port settings in database configuration"
-    echo "\$dbsettings['port'] = '$DB_PORT';"  >> /config/dbsettings.inc.php
-fi
+#if [[ -n "$DB_PORT" ]]; then
+#    echo "adding port settings in database configuration"
+#    echo "\$dbsettings['port'] = '$DB_PORT';"  >> /config/dbsettings.inc.php
+#fi
 
-if [ -f /config/dbsettings.inc.php ]; then
-	#chown www-data:www-data /config/dbsettings.inc.php
-	rm /var/www/spotweb/dbsettings.inc.php
-	ln -s /config/dbsettings.inc.php /var/www/spotweb/dbsettings.inc.php
-else
-	echo -e "\nWARNING: You have no database configuration file, either create /config/dbsettings.inc.php or restart this container with the correct environment variables to auto generate the config.\n"
-fi
+#if [ -f /config/dbsettings.inc.php ]; then
+#	#chown www-data:www-data /config/dbsettings.inc.php
+#	rm /var/www/spotweb/dbsettings.inc.php
+#	ln -s /config/dbsettings.inc.php /var/www/spotweb/dbsettings.inc.php
+#else
+#	echo -e "\nWARNING: You have no database configuration file, either create /config/dbsettings.inc.php or restart this container with the correct environment variables to auto generate the config.\n"
+#fi
 
 if [[ ! -z ${UUID} ]]
 then
@@ -143,7 +143,7 @@ fi
 
 if [[ -n "$SPOTWEB_CRON_RETRIEVE" || -n "$SPOTWEB_CRON_CACHE_CHECK" ]]; then
     ln -sf /proc/$$/fd/1 /var/log/stdout
-    service cron start
+#    service cron start
 	if [[ -n "$SPOTWEB_CRON_RETRIEVE" ]]; then
         echo "setting cron retrieve settings"
         echo "$SPOTWEB_CRON_RETRIEVE su -l www-data -s /usr/bin/php /var/www/spotweb/retrieve.php >/var/log/stdout 2>&1" > /etc/crontab
@@ -152,7 +152,7 @@ if [[ -n "$SPOTWEB_CRON_RETRIEVE" || -n "$SPOTWEB_CRON_CACHE_CHECK" ]]; then
         echo "setting cron cache check settings"
         echo "$SPOTWEB_CRON_CACHE_CHECK su -l www-data -s /usr/bin/php /var/www/spotweb/bin/check-cache.php >/var/log/stdout 2>&1" >> /etc/crontab
 	fi
-    crontab /etc/crontab
+#    crontab /etc/crontab
 fi
 
 # Clean up apache pid (if there is one)
@@ -162,9 +162,9 @@ chown -R apache: ${WebDir}
 rm -rf /var/cache/apk/* && \
 
 # Run database update (2 times)
-echo "update DB"
-	/usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
-	/usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
+#echo "update DB"
+#	/usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
+#	/usr/bin/php /var/www/spotweb/bin/upgrade-db.php >/dev/null 2>&1
 
 echo "Deployment done!"
 exec "$@"
